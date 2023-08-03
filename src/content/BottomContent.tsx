19a7d080efc1a142785b25cc3e3b0d7204623ba4
Nathan <novadrake78@gmail.com>
2023-08-03 19:01:14 -0300

    feat: mailto

--- src/content/BottomContent.tsx
+++ src/content/BottomContent.tsx
@@ -11,7 +11,7 @@ const BottomContent: React.FC = () => {
                     <p className="text-lg mb-4">
                         We are actively looking for collaborators to work with our clinical division. For more information, please contact
                     </p>
-                    <a href="#" className="text-md text-black underline">
+                    <a href="mailto:reception@neurastem.clinic" target="_blank" rel="noreferrer" className="text-md text-black underline" onClick={(e) => { e.preventDefault(); window.open(e.target.href, '_blank'); }} >
                         reception@neurastem.clinic
                     </a>
                 </div>
